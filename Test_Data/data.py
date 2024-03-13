39e21b7e87547cf9f1ff15452f35bf70c72b90c8
vinoth58543 <145655177+vinoth58543@users.noreply.github.com>
2024-03-13 15:14:42 +0530

    Add files via upload

---
+++ Test_Data/data.py
@@ -0,0 +1,19 @@
+class Data:
+    url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    dashboard_url = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
+    username = "Admin"
+    password = "admin123"
+
+#data for invalid login
+
+    invalid_password = "Invalid Password"
+
+#data for add employee
+    
+    first_Name = "vinoth"
+    last_Name ="M"
+    Employee_Id = "58543"
+
+#data for delete employee
+    
+    Employee_Id = "58543"
