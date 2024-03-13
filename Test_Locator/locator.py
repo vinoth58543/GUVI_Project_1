d27c4dfdc7adf48176fcc852cbf8d37bb7a5db65
vinoth58543 <145655177+vinoth58543@users.noreply.github.com>
2024-03-13 15:15:11 +0530

    Add files via upload

---
+++ Test_Locator/locator.py
@@ -0,0 +1,23 @@
+class Locators:
+    #locators for login
+    username_input_box = "username"
+    password_input_box = "password"
+    submit_button = '//html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
+
+
+
+    #locators for add employee
+    PIM ='//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a'
+    Add_Employee ='//*[@id="app"]/div[1]/div[1]/header/div[2]/nav/ul/li[3]/a'
+    first_name = "firstName"
+    last_name = "lastName"
+    Employee_Id ='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/input'
+    save_button ='//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[2]/button[2]'
+
+    #locators for delete employee
+    Employees_list ='//*[@id="app"]/div[1]/div[1]/header/div[2]/nav/ul/li[2]/a'
+    Employee_ID = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[2]/div/div[2]/input'
+    search_button ='//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[2]/button[2]'
+    Trash ='//*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div/div/div[9]/div/button[1]/i'
+    delete = '//*[@id="app"]/div[3]/div/div/div/div[3]/button[2]'
+
